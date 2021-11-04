79c2a940cc062ff4450cacece6c442a7f53fbad9
bartkozlo <bartkozlo@gmail.com>
2021-11-04 15:31:13 +0100

    Add the option to send an order to API

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import styles from './OrderSummary.module.scss';
 import PropTypes from 'prop-types';
+
+import styles from './OrderSummary.module.scss';
 import { calculateTotal } from '../../../utils/calculateTotal';
 import { formatPrice } from '../../../utils/formatPrice';
 
